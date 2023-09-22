4741cc4a2e9fb8c43a5f80a3e03242973a4f0c39
Maximiliano Delfino <maximiliano.delfino@radiumrocket.com>
2023-09-22 14:06:17 -0300

    Setup Redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import React from 'react';
 
 import logo from './assets/logo.svg';
 import Layout from './components/layout';
+import Cart from './features/Cart';
+import Counter from './features/Counter';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Cart {<Cart />}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Layout>
           <p>This is a children</p>
+          <Counter />
         </Layout>
       </header>
     </div>
